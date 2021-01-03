0df33b80339a31dcaae7e3771baa8ae6d6eab7c2
999160 <m.ahmed.abutalib@gmail.com>
2021-01-03 14:34:48 +0400

    testing repositories

--- business/src/test/java/com/naggaro/newsapp/business/news/usecases/GetMostViewedNewsTest.kt
+++ business/src/test/java/com/naggaro/newsapp/business/news/usecases/GetMostViewedNewsTest.kt
@@ -26,7 +26,24 @@ class GetMostViewedNewsTest {
     private lateinit var params: GetMostViewedNews.Params
 
     @Test
-    fun run() = runBlocking {
+    fun `usecase returns data when given by repo`() = runBlocking {
+        val list: List<NewsView> = mock()
+        val result: Either.Right<List<NewsView>> = mock()
+        Mockito.`when`(result.b).thenReturn(list)
+
+        Mockito.`when`(newsRepository.news(ArgumentMatchers.anyString(),ArgumentMatchers.anyInt())).thenReturn(result)
+        val getNews = GetMostViewedNews(CoroutineScope(Dispatchers.IO + Job()), newsRepository)
+        Mockito.`when`(getNews.run(params)).thenReturn(result)
+
+        getNews.invoke(params){
+            assertThat(it).isEqualTo(result)
+            assertThat(result.b).isNotNull()
+        }
+
+    }
+
+    @Test
+    fun `usecase returns error when reported by repo`() = runBlocking {
         val list: List<NewsView> = mock()
         val result: Either.Right<List<NewsView>> = mock()
         Mockito.`when`(result.b).thenReturn(list)
