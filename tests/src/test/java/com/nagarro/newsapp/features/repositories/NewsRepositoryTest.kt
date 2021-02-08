1830d3333e763b6c1c76ac0709d8af904c2d9ee1
mahmed1987 <m.ahmed.abutalib@gmail.com>
2021-02-08 17:27:15 +0400

    NewsRepositoryTest.kt was created

--- tests/src/test/java/com/nagarro/newsapp/features/repositories/NewsRepositoryTest.kt
+++ tests/src/test/java/com/nagarro/newsapp/features/repositories/NewsRepositoryTest.kt
@@ -11,7 +11,6 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import com.google.common.truth.Truth.assertThat
 import org.koin.core.context.startKoin
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.inject
